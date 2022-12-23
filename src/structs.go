b2f729886ee903ba5d537f50ef011d1f994822df
Tainted06 <80160851+Tainted06@users.noreply.github.com>
2022-12-23 07:28:23 +0300

    Update config.json and structs.go

--- src/structs.go
+++ src/structs.go
@@ -5,21 +5,24 @@ import (
 )
 
 type Checker struct {
-	Client http.Client
-	Invalid		int
-	Locked		int
-	Token 		string
-	Valid		int
-	Resp		bool
-	All			int
+	Client  http.Client
+	Invalid int
+	Flagged int
+	Locked  int
+	Token   string
+	Valid   int
+	Resp    bool
+	All     int
 }
 
-type structs struct {
-
+type Config struct {
+	CheckFlagged bool `json:"check_flagged"`
+	Offset       int  `json:"offset_ms"`
 }
 
 var (
-	z = "\033[36m"
-	url = "https://discord.com/api/v9/users/@me/affinities/guilds"
-	c, r, g, bg, rb, gr, u =  "\u001b[30;1m", "\033[39m", "\033[32m", "\u001b[34;1m", "\u001b[0m", "\u001b[30;1m", "\u001b[4m"
+	z                      = "\033[36m"
+	url                    = "https://discord.com/api/v9/users/@me/affinities/guilds"
+	flagged_url            = "https://discord.com/api/v9/users/@me"
+	c, r, g, bg, rb, gr, u = "\u001b[30;1m", "\033[39m", "\033[32m", "\u001b[34;1m", "\u001b[0m", "\u001b[30;1m", "\u001b[4m"
 )
